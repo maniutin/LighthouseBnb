7c00b9ed5cd26ad29e81a1d00a133485bf78a39f
Nick Maniutin <maniutin@gmail.com>
2021-03-04 20:32:46 +0000

    get minimum and maximum costs in $ instead of c

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -148,11 +148,11 @@ const getAllProperties = function (options, limit = 10) {
   queryString += `AND owner_id = $${queryParams.length} `;
   } 
   if (options.minimum_price_per_night){
-    queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.minimum_price_per_night*100}`);
     queryString += `AND cost_per_night >= $${queryParams.length}\n`
   }
   if (options.maximum_price_per_night){
-    queryParams.push(`${options.maximum_price_per_night}`);
+    queryParams.push(`${options.maximum_price_per_night*100}`);
     queryString += `AND cost_per_night <= $${queryParams.length}`
   }
   queryString += `
